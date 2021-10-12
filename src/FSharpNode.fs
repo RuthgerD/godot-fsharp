bde826f942dcb96cb20e8d2b0281c23a2e8d8a9e
Ruthger Dijt <ruthgerdijt@gmail.com>
2021-10-12 22:02:59 +0200

    test

--- src/FSharpNode.fs
+++ src/FSharpNode.fs
@@ -1,8 +1,29 @@
 ﻿namespace MoonoF
 
 open Godot
+open System
 
 type FSharpNode() =
     inherit Node()
 
-    override _._Ready() = GD.Print "Hello from F#"
+    override _._Ready() =
+        GD.Print "Hello from F#"
+
+        // Who invented exceptions??
+
+        let opt = Some()
+
+        match opt with
+        | Some _ -> GD.Print "Sound!"
+        | None -> GD.Print "Unsound!"
+
+
+        // Functional shiz!
+
+        let list = [ 'a' .. 'z' ]
+
+        list
+        |> List.map Char.ToUpper
+        |> List.iteri (fun i c -> GD.Print(c, i))
+
+        ()
